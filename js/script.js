21b1483ab2a1b193b6ed7882ac6ff29c74e51e1e
king644s <sonivatsal3@gmail.com>
2024-03-10 16:54:54 +0530

    thankyou page added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-  target: "#navbar-example3",
-});
-
 // $(document).ready(function(){
 // 	$('.').click(function(){
 // 		$(this).toggleClass('active');
